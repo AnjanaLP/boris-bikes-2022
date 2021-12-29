245a2a5a2ca8cfa843459cae60467efb5d98de42
AnjanaLP <anjana.lpn@gmail.com>
2021-12-29 12:58:52 +0000

    User Story 2 As a person, So that I can use a good bike, I'd like to see if a bike is working

--- spec/feature/user_stories_spec.rb
+++ spec/feature/user_stories_spec.rb
@@ -7,4 +7,12 @@ describe 'User Stories' do
     station = DockingStation.new
     expect{ station.release_bike }.not_to raise_error
   end
+
+  # As a person,
+  # So that I can use a good bike,
+  # I'd like to see if a bike is working
+  it 'a person can see if a bike is working' do
+    bike = Bike.new
+    expect { bike.working? }.not_to raise_error
+  end
 end
